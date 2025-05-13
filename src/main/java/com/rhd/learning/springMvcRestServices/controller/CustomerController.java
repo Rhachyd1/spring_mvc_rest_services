e6fe8d76159cc4a1fd8abf4aacf9d7986303d792
Rhachyd Silveira <32617980+Rhachyd1@users.noreply.github.com>
2025-05-13 19:37:48 -0300

    cleaning

--- src/main/java/com/rhd/learning/springMvcRestServices/controller/CustomerController.java
+++ src/main/java/com/rhd/learning/springMvcRestServices/controller/CustomerController.java
@@ -2,7 +2,7 @@ package com.rhd.learning.springMvcRestServices.controller;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
